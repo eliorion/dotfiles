ad818c23d5c8c043ce5f2a7b4b8dd83fc0c1500e
Eliorion <samuel.kharitonoff@edu.esiee.fr>
2025-12-30 14:17:15 +0000

    Change install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,8 @@
 set -euo pipefail
 
 if ! command -v chezmoi >/dev/null; then
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/eliorion/dotfiles.git
+
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:eliorion/dotfiles.git
 fi
-
-chezmoi apply
-
-/scripts/setup || true
-
-
-echo "=== Installation terminée ==="
 
 exit 0
