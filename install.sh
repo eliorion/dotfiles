036842f220a069892900ef8b66fae2d923d9ed3e
Eliorion <samuel.kharitonoff@edu.esiee.fr>
2025-12-30 21:34:22 +0100

    Delete the warning of exit wihout 0

--- install.sh
+++ install.sh
@@ -7,6 +7,4 @@ if ! command -v chezmoi >/dev/null; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/eliorion/dotfiles.git 
 fi
 
-$HOME/.local/bin/chezmoi apply
-
 exit 0
