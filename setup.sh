17998d491729225b3516e1d3b72d8b33bb19eac1
Eliorion <samuel.kharitonoff@edu.esiee.fr>
2025-12-30 12:52:32 +0000

    Change setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if ! command -v chezmoi >/dev/null; then
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:eliorion/dotfiles.git 
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/eliorion/dotfiles.git
 fi
 
 echo "=== Installation terminée ==="
